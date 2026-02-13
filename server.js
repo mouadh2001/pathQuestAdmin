536426e857b0c142936069217e55be577530d683
mouadh <wannassii.mouadh@gmail.com>
2026-02-13 19:49:44 +0100

    admin app

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const __dirname = path.dirname(__filename);
 const app = express();
 
 connectDB();
-app.use(express.static(path.join(__dirname, "admin-aclient/public")));
+app.use(express.static(path.join(__dirname, "admin-client/public")));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "admin-client/public/dashboard.html"));
